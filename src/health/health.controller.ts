c42ee3f2a27ab89c665b7b1f6da02dd24ae500b3
wujunbao1963 <wujunbao@hotmail.com>
2025-12-31 21:17:46 -0700

    fix /api/health

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { HealthService } from './health.service';
 
-@Controller('health')
+@Controller('api/health')
 export class HealthController {
   constructor(private readonly healthService: HealthService) {}
 
